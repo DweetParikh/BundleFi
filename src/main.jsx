be2bb2e334ee4a588554049a6565786f795caa2c
dweet <dweet.parikh16@gmail.com>
2026-03-27 18:47:24 +0530

    updates live data

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import { PhantomWalletAdapter }  from '@solana/wallet-adapter-phantom'
 import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare'
 import { clusterApiUrl } from '@solana/web3.js'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 import '@solana/wallet-adapter-react-ui/styles.css'
 import './index.css'
